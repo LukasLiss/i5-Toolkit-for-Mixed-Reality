797a47cfc7402ddc471a755eb777524fb52fb07c
Lukas Liß <lukas.liss@rwth-aachen.de>
2021-03-09 21:26:15 +0100

    added asa features

---
+++ Assets/Editor/ASAHelperEditor.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Azure.SpatialAnchors.Unity;
+using Microsoft.MixedReality.Toolkit;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[CustomEditor(typeof(AnchorModuleScript))]
+public class AnchorModuleScriptEditor : Editor
+{
+    override public void OnInspectorGUI()
+    {
+        var AncModScript = target as AnchorModuleScript;
+
+        //EditorGUILayout.BeginHorizontal();
+        //GUILayout.Label("Azure Spatial Anchor Connection Settings");
+        //AncModScript.UseOwnAnchorManager = GUILayout.Toggle(AncModScript.UseOwnAnchorManager, "Do you want to use a custom ASA Manager?");
+        //EditorGUILayout.EndHorizontal();
+
+        if (AncModScript.UseOwnAnchorManager)
+        {
+            EditorGUILayout.BeginHorizontal();
+            AncModScript.ASAManager = EditorGUILayout.ObjectField(AncModScript.ASAManager, typeof(SpatialAnchorManager), true) as SpatialAnchorManager;
+            EditorGUILayout.EndHorizontal();
+        }
+        else
+        {
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Label("Account Id:");
+            AncModScript.ASAAccountId = EditorGUILayout.TextField(AncModScript.ASAAccountId);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Label("Account Key:");
+            AncModScript.ASAAccountKey = EditorGUILayout.TextField(AncModScript.ASAAccountKey);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Label("Account Domain:");
+            AncModScript.ASAAccountDomain = EditorGUILayout.TextField(AncModScript.ASAAccountDomain);
+            EditorGUILayout.EndHorizontal();
+        }
+
+        ARAnchorManager arFoundationAnchorManager = GameObject.FindObjectOfType<ARAnchorManager>();
+        if(arFoundationAnchorManager == null)
+        {
+            GUIStyle textStyle = EditorStyles.label;
+            textStyle.wordWrap = true;
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Label("It seems like there is no AR Foundation Anchor Manager in the Scene. In the case that this scene is loaded additionally to a scene with an AR Anchor Manger that is no problem. But if not than you should use the following button to set up the scene correctly.", textStyle);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            if(GUILayout.Button("Setup Scene for ASA"))
+            {
+                //Try to find Mixed Reality Toolkit
+                MixedRealityToolkit mrtkInScene = FindObjectOfType<MixedRealityToolkit>();
+                if(mrtkInScene != null)
+                {
+                    mrtkInScene.gameObject.AddComponent<ARAnchorManager>();
+                    Debug.Log("AR Anchor Manager added to the Mixed Reality Toolkit object in the scene.");
+                }
+                else
+                {
+                    AncModScript.gameObject.AddComponent<ARAnchorManager>();
+                    Debug.LogWarning("We were not able to locate the Mixed Reality Toolkit in the Scene. Therefore we added the AR Anchor Manager directly on the object that has the ASA Helper component.");
+                }
+
+            }
+            EditorGUILayout.EndHorizontal();
+
+        }
+
+    }
+}
---
+++ Assets/Scenes/ASA_TestScene/ASA_Test_Scene_Script.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ASA_Test_Scene_Script : MonoBehaviour
+{
+    /// <summary>
+    /// The Anchor Module script that handles to usage of ASA.
+    /// </summary>
+    [SerializeField]
+    [Tooltip("The Anchor Module script that handles to usage of ASA.")]
+    public AnchorModuleScript anchorModuleScript;
+
+    /// <summary>
+    /// The Object, which position is stored in ASA and that is repositioned when a old position is loaded.
+    /// </summary>
+    [SerializeField]
+    [Tooltip("The Object, which position is stored in ASA and that is repositioned when a old position is loaded.")]
+    public GameObject objectToSavePositionOf;
+
+    /// <summary>
+    /// The Account Id of the ASA instance.
+    /// </summary>
+    [SerializeField]
+    [Tooltip("The Account Id of the ASA instance.")]
+    public string spatialAnchorsAccountId;
+
+    /// <summary>
+    /// The Account Key of the ASA instance.
+    /// </summary>
+    [SerializeField]
+    [Tooltip("The Account key of the ASA instance.")]
+    public string spatialAnchorsAccountKey;
+
+    /// <summary>
+    /// The Account Domain of the ASA instance.
+    /// </summary>
+    [SerializeField]
+    [Tooltip("The Account Domain of the ASA instance.")]
+    public string spatialAnchorsAccountDomain;
+
+    /// <summary>
+    /// The variable that stores the identifier of a created anchor, so that the last created anchor can be loaded again.
+    /// </summary>
+    private string anchorIdentifier = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public async void SavePosition()
+    {
+        await anchorModuleScript.StartAzureSession(spatialAnchorsAccountId, spatialAnchorsAccountKey, spatialAnchorsAccountDomain);
+        Debug.Log("Session Started");
+        anchorIdentifier = await anchorModuleScript.CreateAzureAnchor(objectToSavePositionOf, DateTimeOffset.Now.AddDays(7));
+        //anchorModuleScript.StopAzureSession();
+    }
+
+    public async void LoadPosition()
+    {
+        await anchorModuleScript.StartAzureSession(spatialAnchorsAccountId, spatialAnchorsAccountKey, spatialAnchorsAccountDomain);
+        anchorModuleScript.FindAzureAnchor(anchorIdentifier, objectToSavePositionOf);
+        //anchorModuleScript.StopAzureSession();
+    }
+}
